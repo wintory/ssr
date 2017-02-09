c3a54348014efa222070c1cc719a1be5f64bcf53
wintory <willyvongga@hotmail.com>
2017-02-09 21:43:09 +0700

    -2

--- src/routes.js
+++ src/routes.js
@@ -3,17 +3,17 @@ import { Route , IndexRoute} from 'react-router'
 
 import ProvinceDetail from './component/province_detail'
 import ActivityDetail from './component/activity_detail'
-import Home from './component/home'1
+import Home from './component/home'
 import App from './component/app'
-import HomeDetail from './home_detail'
+import HomeDetail from './component/home_detail'
 
 export default (
-<Route path="/" component={App}>
-      <Route path="home" component={Home}>
-        <Route path="home_detail" component={HomeDetail}></Route>
-        <Route path="province_detail" component={ProvinceDetail}> </Route>
-        <Route path="activity_detail" component={ActivityDetail} ></Route>
-      </Route>
-</Route>
+  <Route path="/" component={App}>
+        <Route path="home" component={Home}>
+          <Route path="home_detail" component={HomeDetail}></Route>
+          <Route path="province_detail" component={ProvinceDetail}> </Route>
+          <Route path="activity_detail" component={ActivityDetail} ></Route>
+          </Route>
+  </Route>
 
 )
